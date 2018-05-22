3c715c95d2fa11c926fcd70ac73a0f0af53a8cf6
zhutengfei1990 <1041885117@qq.com>
2018-05-22 12:08:52 +0800

    delete test.js

--- test.js
+++
@@ -1,7 +0,0 @@
-bug fixed
-new new new 
-hello world
-this is new add content
-Creating a modify new branch is quick AND simple.
-Creating  aaaa  a new branch is quick AND simple.
-console.log("this is a test file");
